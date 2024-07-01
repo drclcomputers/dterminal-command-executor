8b624aaf71bacc17d2b653338f28c86b73096a12
drclcomputers <132164125+drclcomputers@users.noreply.github.com>
2024-07-01 20:53:19 +0300

    main

--- dterminal-command-executor/main.cpp
+++ dterminal-command-executor/main.cpp
@@ -4,9 +4,17 @@
 
 #ifdef _WIN32
 #include<direct.h>
+#define CLEARSCR "cls"
 #else
 #include<unistd.h>
+#define CLEARSCR "clear"
 #endif
+
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define WHITE "\033[37m"
+#define BLUE "\033[34m"
 
 #ifdef _WIN32
 bool ok = 1;
@@ -15,19 +23,20 @@ bool ok = 1;
 using namespace std;
 
 int main() {
-	cout << "DTerminal\n\n";
+	system(CLEARSCR);
+	cout << BLUE << "DTerminal\n\n";
 	char command[1001];
 	while (strcmp(command, "exit") != 0) {
-		cout << "shell > ";
+		cout << GREEN << "shell > " << WHITE;
 		cin.getline(command, 1000);
 		if (system(command)) {
 			cout << '\n';
 			continue;
 		}
-		else if (strncmp(command, "cd..", 4) == 1 && ok) {
-			cout << "Use 'cd ..' instead of 'cd..'\n";
+		else if (strncmp(command, "cd..", 4) == 0 && ok) {
+			cout << RED << "Use 'cd ..' instead of 'cd..'\n" << WHITE;
 		}
-		else if (strncmp(command, "cd", 2) == 1)
+		else if (strncmp(command, "cd", 2) == 0)
 			chdir(command + 3);
 
 		cout << '\n';
